8d8dbdcd25de1e5ed12a13545edfaccdc1f90b5c
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-09-15 14:38:03 +0700

    add: cell30/ex02

---
+++ discovery_piscine/cell30/ex02/i_got_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    input("What you gotta say? : ")
+
+    while True:
+        msg = input("I got that! Anything else? : ")
+        if (msg == "STOP"):
+            break
+
+main()
