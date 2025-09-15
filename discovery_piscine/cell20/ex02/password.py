efff39ce3c352e740950adfc3b84655cbff575be
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-09-15 13:31:19 +0700

    add: cell20/ex02

---
+++ discovery_piscine/cell20/ex02/password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+input_pwd = input("Enter the password: ")
+
+if input_pwd == password:
+    print("ACCESS GRANTED.")
+else:
+    print("ACCESS DENIED.")
