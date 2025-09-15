ac400887c4253c07a49edc402b1dcaec126a26eb
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-09-15 14:45:58 +0700

    add cell40/ex02

---
+++ discovery_piscine/cell40/ex02/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+current_age = int(input("Please tell me your age: "))
+print(f"You are currently {current_age} years old.")
+
+print(f"In 10 years, you will be {current_age + 10} years old."
+      f"\nIn 20 years, you will be {current_age + 20} years old."
+      f"\nIn 30 years, you will be {current_age + 30} years old.")
